f74348a0f7142f1a17c82700aaafb1e07340f663
Anthony Mottot <anthonymottot@yahoo.fr>
2015-10-25 00:41:11 +0200

    Initialization BillaCrawler

---
+++ crawl/src/test/java/xyz/wheretolive/crawl/billa/BillaTest.java
@@ -0,0 +1,15 @@
+package xyz.wheretolive.crawl.billa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BillaTest implements IBilla {
+
+    @Test
+    public void getBillaRegionList() {
+
+    }
+
+}

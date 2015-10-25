d187cd373da54fe92cca3e4d139adc6436b9aece
Anthony Mottot <anthonymottot@yahoo.fr>
2015-10-25 15:12:06 +0100

    Initialization BillaCrawler

--- crawl/src/test/java/xyz/wheretolive/crawl/billa/IBilla.java
+++ crawl/src/test/java/xyz/wheretolive/crawl/billa/IBilla.java
@@ -9,10 +9,4 @@ import java.util.Set;
  */
 public interface IBilla {
     String BILLA_SHOPS_URL = "https://www.billa.cz/billa.controls/filialfinder/maps/pinpointv3/pinpoint3.32.html?fx=&dataUrl=https://service.cz.rewe.co.at/filialservice/filialjson.asmx/getfilialenjson?shopcd=%22CZ%22&language=CZE&country=CZ";
-
-    String BILLA_SHOPS_CONTAINTER = "//div[@id='WW_myList_control_content']";
-    String BILLA_SHOPS_REGION_LIST = "//div[@id='WW_myList_control_content']/a";
-    String BILLA_SHOPS_REGION_NAME_LIST = "//div[@id='WW_myList_control_content']/a//span";
-
-    Set<String> BILLA_SHOPS_REGION_A_LIST = new LinkedHashSet<>();
 }

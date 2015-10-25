3c07223b287ec5b51ea55a7f2082f677aa28c2ee
Anthony Mottot <anthonymottot@yahoo.fr>
2015-10-25 15:31:32 +0100

    BillaCrawler done

--- crawl/src/main/java/xyz/wheretolive/crawl/BillaCrawler.java
+++ crawl/src/main/java/xyz/wheretolive/crawl/BillaCrawler.java
@@ -1,12 +1,40 @@
 package xyz.wheretolive.crawl;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import xyz.wheretolive.core.domain.Coordinates;
 import xyz.wheretolive.core.domain.MapObject;
+import xyz.wheretolive.core.geocoding.GoogleGeocoder;
+import xyz.wheretolive.crawl.pageObject.BillaMap;
+
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BillaCrawler implements Crawler {
     
     @Override
     public Collection<MapObject> crawl() {
-        return null;
+
+        WebDriver webDriver = new ChromeDriver();
+
+        BillaMap billaMap = new BillaMap(webDriver);
+        List<String> billaRegion = billaMap.getBillaRegion();
+        List<String> globalBillaShops = new LinkedList<>();
+        List<MapObject> toReturn = new LinkedList<>();
+
+        for ( String currentRegion : billaRegion ) {
+            billaMap.get(BILLA_SHOPS_URL);
+            billaMap.clickRegion(currentRegion);
+            List<String> billaShop = billaMap.getAddresses();
+            globalBillaShops.addAll(billaShop);
+        }
+
+        for ( String currentShopAddress : globalBillaShops ) {
+            GoogleGeocoder geocoder = new GoogleGeocoder();
+            toReturn.add(new MapObject(geocoder.translate(currentShopAddress)));
+        }
+
+        return toReturn;
     }
 }
--- crawl/src/main/java/xyz/wheretolive/crawl/Crawler.java
+++ crawl/src/main/java/xyz/wheretolive/crawl/Crawler.java
@@ -4,6 +4,7 @@ import xyz.wheretolive.core.domain.MapObject;
 import java.util.Collection;
 
 public interface Crawler {
-    
+
+    String BILLA_SHOPS_URL = "https://www.billa.cz/billa.controls/filialfinder/maps/pinpointv3/pinpoint3.32.html?fx=&dataUrl=https://service.cz.rewe.co.at/filialservice/filialjson.asmx/getfilialenjson?shopcd=%22CZ%22&language=CZE&country=CZ";
     Collection<MapObject> crawl();
 }

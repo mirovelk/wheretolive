a2ecb2ecf5b784ee5b6249d0bb68e04bd070c453
Matej Ferenc <Matej.Ferenc@4finance.com>
2015-11-12 21:22:21 +0100

    stuff

--- rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
+++ rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @SpringBootApplication
 @ComponentScan("xyz.wheretolive")
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {

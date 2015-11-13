0cff3ad5dc5f7bf9355a7f55a7c45ee59481b829
Matej Ferenc <Matej.Ferenc@4finance.com>
2015-11-13 14:26:15 +0100

    Revert "trying to fix startup"
    This reverts commit 6e1265e09671f5ee03d1875e1135871e6999b201.

--- rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
+++ rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
@@ -2,9 +2,6 @@ package xyz.wheretolive.rest.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,11 +12,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-        return factory;
     }
 }

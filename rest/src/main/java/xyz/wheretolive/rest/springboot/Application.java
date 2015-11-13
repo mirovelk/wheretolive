6e1265e09671f5ee03d1875e1135871e6999b201
Matej Ferenc <Matej.Ferenc@4finance.com>
2015-11-13 14:09:40 +0100

    trying to fix startup

--- rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
+++ rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
@@ -2,6 +2,9 @@ package xyz.wheretolive.rest.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,5 +15,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        return factory;
     }
 }

40eaa6c42149d61b52113f9c44e6a80c2522d742
Matej Ferenc <Matej.Ferenc@4finance.com>
2015-11-19 19:40:34 +0100

    tralala

--- rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
+++ rest/src/main/java/xyz/wheretolive/rest/springboot/Application.java
@@ -1,6 +1,10 @@
 package xyz.wheretolive.rest.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,10 +12,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ComponentScan("xyz.wheretolive")
 @EnableScheduling
-public class Application {
+public class Application implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+
+    private static Logger logger = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
 //        SpringApplication.run(Application.class, args);
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+        context.start();
+    }
+
+    @Override
+    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+        int port = event.getEmbeddedServletContainer().getPort();
+        logger.info("^^^^^^^^^^^^^^^^^^^^^^^ server listening on port " + port + "^^^^^^^^^^^^^^^^^^^^^^^");
     }
 }

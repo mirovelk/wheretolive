1c0e02ba9a5fe4db769e27c50f0e93b2057662e7
Matej Ferenc <Matej.Ferenc@4finance.com>
2016-04-11 12:08:09 +0200

    spring setup

---
+++ rest/src/main/java/xyz/wheretolive/rest/spring/Application.java
@@ -0,0 +1,30 @@
+package xyz.wheretolive.rest.spring;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Application extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    private static Logger logger = LogManager.getLogger(Application.class);
+    
+//    public static void main(String[] args) {
+//        logger.info("main method started");
+//        SpringApplication.run(Application.class, args);
+//    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/*"};
+    }
+}
---
+++ rest/src/main/java/xyz/wheretolive/rest/spring/WebConfig.java
@@ -0,0 +1,32 @@
+package xyz.wheretolive.rest.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan("xyz.wheretolive")
+@EnableScheduling
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/public/");
+    }
+}

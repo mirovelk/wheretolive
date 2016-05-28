4c6ad14cde87082a9b14c1472e25241b5fe7cfb0
cizmicka <pavol.cizmarik@hotmail.sk>
2016-05-28 10:20:00 +0200

    quickfix

--- rest/src/main/resources/public/js/map.js
+++ rest/src/main/resources/public/js/map.js
@@ -255,14 +255,7 @@ function loadRealities() {
                 };
                 item.icon = icon;
                 if ($.inArray(uniqueId, housingMeta.favoriteHousingIds) != -1) {
-                var icon = {
-                    path: 'M18.237,60.574l23.096,19.578l-7.183,29.438c-1.411,5.491,4.648,9.998,9.575,6.901L69.5,100.572l25.774,15.916  c4.79,2.955,10.844-1.408,9.576-6.902l-7.184-29.435l23.099-19.579c4.363-3.661,2.111-10.844-3.662-11.267l-30.282-2.255  L75.414,19.025c-2.112-5.211-9.577-5.211-11.832,0L52.175,47.051l-30.281,2.255C16.124,49.872,13.869,56.913,18.237,60.574z',
-                    scale: 1,
-                    fillColor: "rgb(255,202,40)",
-                    strokeColor: "rgb(255,202,40)",
-                    fillOpacity: 1
-                }
-                    icon = icon;
+                    icon = '../img/favorite.png';
                 }
                 createHousingMarker(item, icon);
             });
